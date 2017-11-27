700dc8af6e65b0e470b79f644a724deeff02a182
mzolfo <32077843+mzolfo@users.noreply.github.com>
2017-11-27 17:30:49 -0600

    almostcaughtup
    raycast still hits fpscontroller also interaction doesnt actually work for some reason.

---
+++ Assets/Scripts/GenericInteractiveObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericInteractiveObject : MonoBehaviour
+{
+
+
+   
+
+    public void DoActivate()
+    {
+        Debug.Log(transform.name + "was activated!");
+    }
+
+}
